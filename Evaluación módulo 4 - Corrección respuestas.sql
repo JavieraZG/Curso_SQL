b7935dd633d8578a6cef772f76bd089496da5d4a
JavieraZG <156841814+JavieraZG@users.noreply.github.com>
2024-01-31 07:50:22 -0300

    Update Evaluación módulo 4 - Corrección respuestas.sql

--- Evaluación módulo 4 - Corrección respuestas.sql
+++ Evaluación módulo 4 - Corrección respuestas.sql
@@ -4,25 +4,32 @@ CORRECCION RESPUESTAS ERRÓNEAS
 Nombre: Javiera Zavala
 */
 
--- Pregunta 1
+
+/*
+Pregunta 1
 -- ¿Qué string obtendrias al aplicar la siguiente función?
--- SUBSTRING ('sql nivel inicial', 7, 13)
+*/
+SUBSTRING ('sql nivel inicial', 7, 13)
 
 -- RESPUESTA: 'VEL INI'
 
-
---Pregunta 3
--- Marca los enunciados correctos respecto al string 'para la vida'
-
+----------------------------------------------------------------------------------------------
+	
+/*
+Pregunta 3
+Marca los enunciados correctos respecto al string 'para la vida'
+*/
 -- RESPUESTA: 
--- SUBSTRING(''para la vida', 9, 4) => 'vida'
--- LEFT(''para la vida', 4) => 'para'
--- RIGHT(''para la vida', 4) => 'vida'
--- SUBSTRING(''para la vida', 9) => 'vida'
+SUBSTRING(''para la vida', 9, 4) => 'vida'
+LEFT(''para la vida', 4) => 'para'
+RIGHT(''para la vida', 4) => 'vida'
+SUBSTRING(''para la vida', 9) => 'vida'
 
---Pregunta 8
-/*Crea direcciones de email para los clientes
-Requisitos
+----------------------------------------------------------------------------------------------
+	
+/*
+Pregunta 8
+Crea direcciones de email para los cliente. Requisitos:
 
 Usa los 3 primeros caractéres del first_name, luego el símbolo punto', luego la inicial del apellido, luego símbolo punto, luego el resto de caractéres del apellido  
 y finalmente concatena el string '@xmail.com'
